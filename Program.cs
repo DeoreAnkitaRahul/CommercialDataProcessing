9315da2c91b79408c9ee4ceb745d4f0b48657718
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-24 12:59:14 +0530

    Welcome Msg Printing

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CommercialDataProcessing
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Commercial Data Processing ");
+          
+        }
+    }
+}
